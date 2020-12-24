0f402077c7d935d7a5100e49b7bcba0fadf57c26
Hua Li <58175242+HLN177@users.noreply.github.com>
2020-12-24 23:02:37 +0000

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ module.exports = {
     mysql: {
         host: '127.0.0.1',
         user: 'root',
-        password: '10181600',
+        password: '123456',
         database: 'webapp',  
         port: 3306,
         charset:"utf8"
